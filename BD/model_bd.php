55609702d800ee2f1c35c7d8d8b58c0aa6cf7035
Loris-Gr <grandchamploris@gmail.com>
2025-01-10 12:31:04 +0100

    ajout give_score et changement de l'init

---
+++ BD/model_bd.php
@@ -0,0 +1,32 @@
+<?php
+
+function init_bd() {
+    include 'auth.php';
+    $pdo = new PDO("mysql:host=servinfo-maria;dbname=DB$name", $name, $pwd);
+    $table= "SCORE";
+    $columns = "Id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, Username VARCHAR( 50 ) NOT NULL, Score INT, Question VARCHAR ( 50 ) " ;
+    $createTable = $pdo->exec("CREATE TABLE IF NOT EXISTS $table ($columns)");
+    return $pdo;
+}
+
+function recreate_db() {
+    include 'auth.php';
+    $pdo = new PDO("mysql:host=servinfo-maria;dbname=DB$name", $name, $pwd);
+    $table= "SCORE";
+    $pdo->exec("DROP TABLE IF EXISTS $table");
+    $columns = "Id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, Username VARCHAR( 50 ) NOT NULL, Score INT, Question VARCHAR ( 50 ) " ;
+    $createTable = $pdo->exec("CREATE TABLE IF NOT EXISTS $table ($columns)");
+    return $pdo;
+    
+}
+
+function give_score($username, $db) {
+    $sql = "SELECT * FROM SCORE WHERE Username = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([$username]);
+    $rows = $stmt->fetchAll();
+    return $rows;
+}
+
+$db = init_bd();
+?>
