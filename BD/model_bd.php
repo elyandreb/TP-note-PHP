6c156533dbf93e43dc2cd089a6c0ab97bd41fcce
Loris-Gr <grandchamploris@gmail.com>
2025-01-16 23:09:00 +0100

    ajout Date_crea

--- BD/model_bd.php
+++ BD/model_bd.php
@@ -4,7 +4,11 @@ function init_bd() {
     include 'auth.php';
     $pdo = new PDO("mysql:host=servinfo-maria;dbname=DB$name", $name, $pwd);
     $table= "SCORE";
-    $columns = "Id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, Username VARCHAR( 50 ) NOT NULL, Score INT, Question VARCHAR ( 50 ) " ;
+    $columns = "
+    Id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, 
+    Username VARCHAR( 50 ) NOT NULL, 
+    Score INT, Question VARCHAR ( 50 ),
+    Date_crea TIMESTAMP DEFAULT CURRENT_TIMESTAMP " ;
     $createTable = $pdo->exec("CREATE TABLE IF NOT EXISTS $table ($columns)");
     return $pdo;
 }
@@ -14,13 +18,17 @@ function recreate_db() {
     $pdo = new PDO("mysql:host=servinfo-maria;dbname=DB$name", $name, $pwd);
     $table= "SCORE";
     $pdo->exec("DROP TABLE IF EXISTS $table");
-    $columns = "Id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, Username VARCHAR( 50 ) NOT NULL, Score INT, Question VARCHAR ( 50 ) " ;
+    $columns = "
+    Id INT( 11 ) AUTO_INCREMENT PRIMARY KEY, 
+    Username VARCHAR( 50 ) NOT NULL, 
+    Score INT, Question VARCHAR ( 50 ),
+    Date_crea TIMESTAMP DEFAULT CURRENT_TIMESTAMP " ;
     $createTable = $pdo->exec("CREATE TABLE IF NOT EXISTS $table ($columns)");
     return $pdo;
     
 }
 
-function give_score($username, $db) {
+function give_user_score($username, $db) {
     $sql = "SELECT * FROM SCORE WHERE Username = ?";
     $stmt = $db->prepare($sql);
     $stmt->execute([$username]);
