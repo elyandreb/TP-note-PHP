fbb2150c2dd1e2b77e0c0f3ff1cac3233a7e107a
elyandreb <elyandreburet72@gmail.com>
2025-01-17 01:39:31 +0100

    fonction validation du quiz implemente

--- Classes/Controller.php
+++ Classes/Controller.php
@@ -25,4 +25,18 @@ class Controller
         }
         echo "</ol><input type='submit' value='Envoyer'></form>";
     }
+
+    public function submitQuiz($answers)
+    {
+        $scoreTotal = 0;
+        $scoreCorrect = 0;
+
+        foreach ($this->questions as $q) {
+            $handler = [QuestionHandlers::class, "answer_{$q["type"]}"];
+            $scoreTotal += $q["score"];
+            $scoreCorrect += call_user_func($handler, $q, $answers[$q["name"]] );
+        }
+        echo "<p>Votre score : $scoreCorrect / $scoreTotal</p>";
+        echo "<a href='index.php'>Recommencer</a>";
+    }
 }
