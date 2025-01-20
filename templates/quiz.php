d6d7cd125e55af589c2b853a3c7d344aa5406a18
elyandreb <elyandreburet72@gmail.com>
2025-01-20 17:35:47 +0100

    template pour choisir le quiz

---
+++ templates/quiz.php
@@ -0,0 +1,7 @@
+<?php
+include __DIR__ . '/header.php';
+
+echo "<h1>Selection du Quiz</h1>";
+echo "<a href='index.php?action=home&quiz=questions'>Quiz1</a><br>";
+echo "<a href='index.php?action=home&quiz=animaux'>Quiz Animaux</a>";
+?>
