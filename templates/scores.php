5ece6a787974b698864633c5c216414b12f60caf
elyandreb <elyandreburet72@gmail.com>
2025-01-21 23:16:19 +0100

    modif template score

--- templates/scores.php
+++ templates/scores.php
@@ -6,15 +6,36 @@ use BD\model_bd;
 $bd = new model_bd();
 $username = $_SESSION['utilisateur'] ?? 'anonymous';
 $scores = $bd->give_user_score($username);
+?>
 
-echo "<h3>Historique de mes scores :</h3>";
-    if (!empty($scores)) {
-        foreach ($scores as $score) {
-            echo "Quiz : {$score['Question']} - Score : {$score['Score']}/{$score['ScoreMax']}<br><br>";
-        }
-    }
-    else {
-        echo "Aucun score disponible. <a href='index.php'>Faire le quiz</a>";
-    }
-echo "<a href='../index.php'> Revenir à l'accueil </a>"
-?> 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mes Scores</title>
+    <link rel="stylesheet" href="styles/scores.css">
+</head>
+<body>
+    <header>
+        <h1>Mes Scores</h1>
+    </header>
+    <main>
+        <section class="scores-container">
+            <?php if (!empty($scores)): ?>
+                <ul class="scores-list">
+                    <?php foreach ($scores as $score): ?>
+                        <li>
+                            Quiz : <?= htmlspecialchars($score['Question']) ?> - 
+                            Score : <?= htmlspecialchars($score['Score']) ?>/<?= htmlspecialchars($score['ScoreMax']) ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php else: ?>
+                <p>Aucun score disponible. <a href="index.php">Faire le quiz</a></p>
+            <?php endif; ?>
+            <a href="../index.php" class="btn">Revenir à l'accueil</a>
+        </section>
+    </main>
+</body>
+</html>
